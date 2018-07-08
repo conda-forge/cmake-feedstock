70f0603f6ec7413242600219babbfc6e038f0a90
Isuru Fernando <isuruf@gmail.com>
2018-07-07 21:29:37 -0600

    Print logs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,7 +15,9 @@ cmake -LAH -G"NMake Makefiles"                               ^
     -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%                        ^
     -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
-if errorlevel 1 exit 1
+
+type CMakeFiles/CMakeOutput.log
+type CMakeFiles/CMakeError.log
 
 cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
