eb4c17f1dd28ca463e632cbfa4773906888dd389
Fred Eisele <fredrick.eisele@gmail.com>
2023-08-11 15:53:01 -0500

    correcting issues with the windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,12 @@
 
 if "%ARCH%"=="32" (set CPU_ARCH=x86) else (set CPU_ARCH=x64)
-curl https://cmake.org/files/v%PKG_VERSION:~0,4%/cmake-%PKG_VERSION%-win%ARCH%-%CPU_ARCH%.zip -o cmake-win.zip
+
+set CMAKE_URL=https://cmake.org/files/v%PKG_VERSION:~0,4%/cmake-%PKG_VERSION%-win%ARCH%-%CPU_ARCH%.zip
+curl "%CMAKE_URL%" -o cmake-win.zip
 7z x cmake-win.zip > nil
 set PATH=%CD%\cmake-%PKG_VERSION%-win%ARCH%-%CPU_ARCH%\bin;%PATH%
 cmake --version
+echo "CMAKE URL: %CMAKE_URL%"
 
 mkdir build && cd build
 
@@ -12,7 +15,7 @@ set CMAKE_CONFIG="Release"
 dir /p %LIBRARY_PREFIX%\lib
 
 
-cmake -LAH -G"NMake Makefiles"                               ^
+cmake -LAH -G"Ninja"                                         ^
     -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%                        ^
     -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                ^
     -DCMAKE_PREFIX_PATH="%PREFIX%"                           ^
