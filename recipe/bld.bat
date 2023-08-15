afee644992120a6db6d25a7559dee79c5ebef2fe
Fred Eisele <fredrick.eisele@gmail.com>
2023-08-15 13:49:21 -0500

    try using cmake-no-system as a dependency

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,4 @@
 
-rem cmake is needed to build cmake
-
-if "%ARCH%"=="32" (set CPU_ARCH=x86) else (set CPU_ARCH=x64)
-rem https://cmake.org/files/v%PKG_VERSION:~0,4%/cmake-%PKG_VERSION%-windows-%ARCH%_%CPU_ARCH%.zip
-set CMAKE_URL=https://cmake.org/files/v3.27/cmake-3.27.2-windows-x86_64.zip
-curl %CMAKE_URL% -o cmake-win.zip
-7z x cmake-win.zip > nil
-rem set PATH=%CD%\cmake-%PKG_VERSION%-windows-%ARCH%-%CPU_ARCH%\bin;%PATH%
-set PATH=%CD%\cmake-3.27.2-windows-x86_64\bin;%PATH%
-cmake --version
-
 rem Build new cmake
 mkdir build
 
